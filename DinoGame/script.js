6fba10dc6a086b74af1f23a5df2f60e0be85c1b2
ωïη†hz <aihtam38@gmail.com>
2025-03-18 19:34:32 -0300

    Add files via upload

--- DinoGame/script.js
+++ DinoGame/script.js
@@ -1,6 +1,14 @@
 const dino = document.querySelector('#dino')
 const cacto = document.querySelector('#cacto')
 const endgame = document.querySelector('#game-over')
+const gameboard = document.querySelector('.game-board')
+let highScoreEL = document.querySelector('#highscore span')
+let scoreEl = document.querySelector('#score span')
+let score = 0
+let highScore = 0
+highScore = parseInt(localStorage.getItem('highScore'))
+
+highScoreEL.textContent = highScore
 
 function jump() {
     if (!dino.classList.contains('jump')) {
@@ -17,7 +25,7 @@ document.addEventListener('keydown', (event) => {
     }
 })
 
-function colisao() {
+function gameloop() {
     let dinoRect = dino.getBoundingClientRect()
     let cactoRect = cacto.getBoundingClientRect()
 
@@ -28,8 +36,16 @@ function colisao() {
     ) {
         endgame.style.display = 'block'
         cacto.style.animation = 'none'
+        gameboard.style.animation = 'none'
+        dino.style.animation = 'none'
         document.removeEventListener('keydown', jump)
+        if (score > highScore) {
+            localStorage.setItem('highScore', score)
+        }
+    } else {
+        score += 10
+        scoreEl.textContent = score
     }
 }
 
-setInterval(colisao, 50)
+setInterval(gameloop, 50)
